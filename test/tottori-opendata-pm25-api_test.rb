50d4f68c5188377ecb96ddc7dbe5dfe337edcf6d
Masayuki Higashino <msyk@hgsn.info>
2017-01-22 10:58:21 +0900

    add unit tests

--- test/tottori-opendata-pm25-api_test.rb
+++ test/tottori-opendata-pm25-api_test.rb
@@ -3,7 +3,11 @@ require 'test_helper'
 class Tottori::OpenData::PM25::ApiTest < Minitest::Test
 
   def test_version
-    refute_nil ::Tottori::OpenData::PM25::API::VERSION
+    refute_nil Tottori::OpenData::PM25::API::VERSION
+  end
+
+  def test_get
+    Tottori::OpenData::PM25::API.get(Date.today)
   end
 
 end
